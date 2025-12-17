dd055275fac144203590400fb8644bac0b0cd0fc
Christopher King <Chris.king@Keyin.com>
2025-12-16 22:12:57 -0330

    Fix CI: use H2 for Spring Boot tests

--- src/test/java/com/aviation/aviation_api/AviationApiApplicationTests.java
+++ src/test/java/com/aviation/aviation_api/AviationApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.aviation.aviation_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AviationApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
